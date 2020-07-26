de9d29506cfddfac147ec5a5ad54005d33c72899
Anusha M <anushahmahadev@gmail.com>
2020-07-26 17:33:50 +0530

    testing

--- force-app/main/default/lwc/gitTestFile/gitTestFile.js
+++ force-app/main/default/lwc/gitTestFile/gitTestFile.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class GitTestFile extends LightningElement {}
+export default class GitTestFile extends LightningElement {
+    /*testing*/
+}
